6f3369446bd8e99e42d33ffebfb92c26ca70c9b4
qhu <phelang.qhu@gmail.com>
2019-07-27 20:59:45 +0200

    Create test cases to create directory level. And create test cases to move a directory.

--- src/test/java/com/filesystem/filesystem/model/DirectoryTest.java
+++ src/test/java/com/filesystem/filesystem/model/DirectoryTest.java
@@ -44,6 +44,25 @@ public class DirectoryTest {
     }
 
     @Test
+    public void testDirectoryLevel() {
+
+        Directory<String> user = home
+                .addDirectory(new Directory<>("hanlie"));
+
+        Directory<String> movies = new Directory<>("Movies");
+        Directory<String> action = new Directory<>("Action");
+
+        user.addDirectory(movies);
+        movies.addDirectory(action);
+
+        Assert.assertEquals("home", 0, home.getLevel());
+        Assert.assertEquals("home/user", 1, user.getLevel());
+        Assert.assertEquals("home/user/Movies", 2, movies.getLevel());
+        Assert.assertEquals("home/user/Movies/Action", 3, action.getLevel());
+
+    }
+
+    @Test
     public void testThatHomeDirectoryExistFromAnyGivenSubDirectory() {
 
         Directory<String> user = home
@@ -131,5 +150,80 @@ public class DirectoryTest {
          */
         Assert.assertNull(user.searchDirectory(user, "Games"));
         Assert.assertEquals("Hobbies", updatedDirectory.getName());
+    }
+
+    @Test
+    public void testThatDirectoryIsMoved() {
+
+        Directory<String> user = home
+                .addDirectory(new Directory<>("hanlie"));
+
+        /**
+         *  Hanlie Directories
+         */
+        Directory<String> movies = new Directory<>("Movies");
+        Directory<String> action = new Directory<>("Action");
+
+        Directory<String> music = new Directory<>("Music");
+        Directory<String> classical = new Directory<>("Classical");
+        Directory<String> rock = new Directory<>("Rock");
+
+        user.addDirectory(movies);
+        user.addDirectory(music);
+
+        movies.addDirectory(action);
+
+        music.addDirectory(classical);
+        music.addDirectory(rock);
+
+
+        /**
+         * Move Movies directory to sub directory Action
+         *
+         * Process not allowed
+         */
+
+        boolean isMoved = movies.moveDirectory(action);
+
+        Assert.assertFalse("Move : home/hanlie/movies -> home/hanlie/Movies/Action", isMoved);
+
+
+        /*******************************************************
+         * *****************************************************
+         * Create a new directory that Movies will be moved To *
+         * *****************************************************
+         *******************************************************/
+
+        Directory<String> toWatchMovies = new Directory<>("To Watch Movies");
+        user.addDirectory(toWatchMovies); // add directory to user
+
+        isMoved = movies.moveDirectory(toWatchMovies);
+
+        Assert.assertTrue("home/hanlie/movies -> home/hanlie/To Watch Movies", isMoved);
+
+        /**
+         * Check that movies NO LONGER exist in home/hanlie/movies
+         * AND
+         * Check that movies EXIST in home/hanlie/To Watch Movies
+         * AND
+         * Check that the sub directories of movies exist
+         */
+
+        Assert.assertEquals(1, toWatchMovies.getSubDirectories().size());
+
+        Assert.assertEquals(
+                "home/hanlie/To Watch Movies/Movies",
+                "Movies",
+                toWatchMovies
+                        .getSubDirectories().get(0)
+                        .getName());
+
+        Assert.assertEquals(
+                "home/hanlie/To Watch Movies/Movies/Action",
+                "Action",
+                toWatchMovies
+                        .getSubDirectories().get(0)
+                        .getSubDirectories().get(0)
+                        .getName());
     }
 }
